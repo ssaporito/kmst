c6f6797c6fe3067f9214d69ca5742d7516d977be
Sandro <sandrosaporito@outlook.com>
2016-12-08 00:49:47 -0200

    minor addition

--- kmst_guess.py
+++ kmst_guess.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-from operator import itemgetter
 from solutions_treat import *
 
 
@@ -27,7 +26,16 @@ def kmst(G,k):
         P(G,v,search_level)
     #print(P_memo)
     P_sorted_v=list(map(lambda x:x[0],sorted(P_memo.items(),key=lambda x:x[1][search_level])))
+    count_v=0
+    sub=nx.Graph()
     print(P_sorted_v)
+    # gotta fix this
+    while True:
+        sub=G.subgraph(P_sorted_v) 
+        if nx.is_connected(sub):
+            print(nx.minimum_spanning_tree(sub))
+            break    
+    return nx.minimum_spanning_tree(sub)
     #sorted(,key=lambda el:el[1])
     #kmst_backtrack(k+1,T,V,E)
 
@@ -40,10 +48,10 @@ def test_kmst():
     #e=[(0,1,1),(0,2,1),(0,3,2),(1,2,10),(2,3,1),(3,1,10),(2,4,1),(3,5,1),(6,2,2),(3,7,1)]
     e=[(0,1,1),(0,2,1),(1,3,10),(2,3,10),(3,4,2),(3,5,2),(4,6,3),(5,6,2)]
     G.add_weighted_edges_from(e)
-    kmst(G,3)    
+    solution=kmst(G,3)    
     #print_solutions(G,get_weighted_solutions(G,solutions))
     try:
-        min_solution=minimum_solution(get_weighted_solutions(G,solutions))
+        min_solution=minimum_solution(get_weighted_solutions(G,[solution]))
         print(min_solution)
         selected=min_solution[0]
         #print(selected)
